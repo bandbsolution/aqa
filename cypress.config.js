f2f7b59e6390869316c8a95a5d02890eaf27cc5e
andriikarachevtsev <andrii.karachevtsev@namecheap.com>
2024-05-29 14:22:21 +0300

    test: config

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,18 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
   e2e: {
     baseUrl: 'https://dev.bonfairplace.com/ua/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+   // browser: 'chrome',
   },
 });
