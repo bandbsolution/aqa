9f2f8e794542da37ffeb48f76a3ee8c0463983ee
andriikarachevtsev <andrii.karachevtsev@namecheap.com>
2024-08-02 02:30:58 +0300

    test: other files

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,6 @@
-const { defineConfig } = require('cypress');
+import { defineConfig } from 'cypress';
 
-module.exports = defineConfig({
+export default defineConfig({
     viewportHeight: 1080,
     viewportWidth: 1920,
     watchForFileChanges: false,
@@ -16,10 +16,10 @@ module.exports = defineConfig({
     },
     e2e: {
         baseUrl: 'https://dev.bonfairplace.com/ua',
-        baseApiUrl: 'https://dev.bonfairplace.com/v1/',
-        setupNodeEvents(on, config) {},
+        setupNodeEvents() {},
         env: {
             MAILSLURP_API_KEY: 'ef2fac5b48dfa754a70c70e63d31dee02a691c11eb25a47741d687a6430bc39d',
+            baseApiUrl: 'https://dev.bonfairplace.com/v1/',
         },
         supportFile: 'cypress/support/e2e.js',
         experimentalRunAllSpecs: true,
